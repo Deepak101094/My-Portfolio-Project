d4cf60b043398f71fdeb610a19c018b68c2d88ec
deepak pandey <deepakpandey101094@gmail.com>
2020-04-27 11:03:29 +0530

    adding routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+//? own component
+import Resume from "./components/Resume";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route exact path="/resume">
+        {" "}
+        <Resume />{" "}
+      </Route>
+      <Route exact path="/">
+        {" "}
+        <App />{" "}
+      </Route>
+    </Switch>
+  </Router>,
+
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
