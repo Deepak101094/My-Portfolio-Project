e413881eefb3bbf8a18c120916d4cb06f5fe40f2
deepak pandey <deepakpandey101094@gmail.com>
2020-04-29 20:27:53 +0530

    creating resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,38 +11,37 @@ import Divider from "@material-ui/core/Divider";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import LinearProgress from '@material-ui/core/LinearProgress';
-import { lighten, createStyles, withStyles, Theme } from '@material-ui/core/styles';
 import Skills from "./Skills";
-
-const BorderLinearProgress = withStyles({
-  root: {
-    height: 15,
-    backgroundColor: lighten('#ff6c5c', 0.5),
-  },
-  bar: {
-    borderRadius: 20,
-    backgroundColor: '#ff6c5c',
-  },
-})(LinearProgress);
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: "4rem"
   },
   paper: {
     background: "grey",
   },
+  leftCard :{
+    background: " #1e2f2f",
+    opacity: "1"
+   },
   avatar: {
     display: "block",
     margin: theme.spacing(1),
     width: theme.spacing(25),
     height: theme.spacing(25),
+  },
+  divider: {
+  background: "black"
+  },
+  text: {
+    color: '#ffffff',
+    textAlign: 'center',
+    margin: "3rem",
   },
   card1: {
     maxWidth: 900,
@@ -53,22 +52,30 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 900,
     height: 300,
     marginBottom: "1rem",
-    background: ' #c3c388',
+    background: '#330011',
+    opacity: "0.8",
     "&hover:": {
-      width: 1000,
-      height:400,
+      // width: 1000,
+      // height:400,
     }
   },
   card3: {
     maxWidth: 900,
     height: "auto",
     marginBottom: "1rem",
-    background: '#c3c388'
+    background: '#330000',
+    opacity: "0.8"
   },
   image: {
     width: "100%",
     height: "auto",
   },
+fontColor: {
+  color: "white",
+  margin: '1rem',
+},
+
+
 }));
 
 const Resume = () => {
@@ -78,19 +85,21 @@ const Resume = () => {
     <div className={classes.root}>
       <Grid container>
         <CssBaseline />
-        <Grid item xs={12} container spacing={2}>
+        <Grid item xs={12} container spacing={0}>
           <Grid item sm={4} xs={12}>
-            <Card>
+            <Card className={classes.leftCard}>
               <Grid container justify="center">
                 <Avatar className={classes.avatar} src={avatar} alt="deepak" />
               </Grid>
+              <Divider className={classes.divider} variant="fullWidth"/>
               <CardContent>
-                <Typography variant="h4">left Card Component</Typography>
+                <Typography className={classes.text} variant="h4"> Deepak Pandey </Typography>
+                <Typography style={{color:'blue', marginBottom: '0.8rem' }} variant="h5"> Web Developer </Typography>
+                <Divider style={{background: "black"}} variant="middle" />
                 <Typography variant="h5">
-                  my name is deepak pandey.my name is deepak pandey.my name is
-                  deepak pandey.my name is deepak pandey.my name is deepak
-                  pandey.my name is deepak pandey.my name is deepak pandey.my
-                  name is deepak pandey.my name is deepak pandey
+                An experienced web developer with a strong interest in projects that require both conceptual and
+                analytical thinking. Fully-committed to designing and developing innovative web-based materials that
+                users will love. Always eager to learn new technologies being introduced in the market.
                 </Typography>
               </CardContent>
             </Card>
@@ -98,7 +107,7 @@ const Resume = () => {
           <Grid item sm={8} xs={12}>
             <Paper className={classes.paper} >
               <Card className={classes.card1}>
-                <Typography variant="h3"> Education </Typography>
+                <Typography style={{ marginLeft: "1rem" }} variant="h2"> Education </Typography>
                 <CardActionArea>
                   <CardMedia
                     className={classes.image}
@@ -108,29 +117,16 @@ const Resume = () => {
                     image={Education}
                     title="Education"
                   />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Education
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
-                    </Typography>
-                  </CardContent>
+                 
                 </CardActionArea>
               </Card>
               <Card className={classes.card2}>
                 <CardContent>
-                  <Typography gutterBottom variant="h3" component="h2">
+                  <Typography style={{color: 'white'}}  gutterBottom variant="h2" component="h2">
                     Experience
                   </Typography>
                   <Divider  variant="fullWidth" light={true} />
-                  <Typography variant="h5">
+                  <Typography className={classes.fontColor} variant="h5">
                     I have 6 month of self experience and I design some website
                     like Resort Booking ,Burger Builder and My portfolio and
                     these websites perfectly working on every size of screen
@@ -139,8 +135,14 @@ const Resume = () => {
                 </CardContent>
               </Card>
               <Card className={classes.card3}>
-              <Typography variant="h3"> Skills </Typography>
-             <Skills />
+              <Typography className={classes.fontColor} variant="h2"> Skill Level </Typography>
+             <Skills skill= "Reactjs" progress= "80" />
+             <Skills skill= "Reduxjs" progress= "70" />
+             <Skills skill= "JavaScript" progress="60" />
+             <Skills skill= "GIT" progress= "60" />
+             <Skills skill= "HTML" progress="80" />
+             <Skills skill= "CSS" progress="65" />
+             <Skills skill= "Material-UI" progress="70" />
               </Card>
             </Paper>
           </Grid>
